8cf8bcba655d035af5ce5bdbe6d9bbd86634fd4c
David Figueroa <figueroada@vcu.edu>
2021-03-11 23:57:42 -0500

    commit to add readme, html, css, and js

---
+++ assets/game.js
@@ -0,0 +1,114 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+
+let questions = [
+    {
+        question: 'How many championships did Michael Jordan win?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '6',
+        choice4: '7',
+        answer: 3,
+    },
+    {
+        question:
+            "What team was Kevin Durant drafted by in 2007?",
+        choice1: "Golden State Warriors",
+        choice2: "Seattle SuperSonics",
+        choice3: "New York Knicks",
+        choice4: "Boston Celtics",
+        answer: 2,
+    },
+    {
+        question: "What NBA Player has the most points in NBA history?",
+        choice1: "Michael Jordan",
+        choice2: "LeBron James",
+        choice3: "Kareem Abdul-Jabbar",
+        choice4: "Kobe Bryant",
+        answer: 3,
+    },
+    {
+        question: "How many NBA teams are there?",
+        choice1: "32",
+        choice2: "26",
+        choice3: "28",
+        choice4: "30",
+        answer: 4,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
